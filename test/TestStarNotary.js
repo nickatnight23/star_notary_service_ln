700c12fc80007e0dfab8a8588917cf13e05d7c73
Tricia <tricianickelberry@gmail.com>
2021-07-14 16:10:45 -0700

    working on tests

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -81,9 +81,17 @@ it('can add the star name and star symbol properly', async() => {
 });
 
 it('lets 2 users exchange stars', async() => {
+    let tokenId = 13;
+    let instance = await StarNotary.deployed();
+
     // 1. create 2 Stars with different tokenId
+    await instance.createStar('Tricia Star Token!', tokenId, {from: accounts[0]});
+    await instance.createStar('Tricia Star Token!', tokenId, {from: accounts[1]});
     // 2. Call the exchangeStars functions implemented in the Smart Contract
+    await instance.exchangeStars(tokenId1, tokenId2, {from: user1});
     // 3. Verify that the owners changed
+    assert.equal(await instance.ownerOf(tokenId1), user2);
+    assert.equal(await instance.ownerOf(tokenId2), user1);
 });
 
 it('lets a user transfer a star', async() => {
